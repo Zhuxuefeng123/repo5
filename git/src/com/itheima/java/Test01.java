7b5ce57fe6c23f0ea0b5a8729fcf74ae4212f473
zhangsan <zhangsan@itheima.com>
2019-05-13 11:20:45 +0800

    把本地工程上传到git

---
+++ git/src/com/itheima/java/Test01.java
@@ -0,0 +1,14 @@
+package com.itheima.java;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+        System.out.println("Hello,git project");
+    }
+}
